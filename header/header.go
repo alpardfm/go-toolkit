ec417b8964f05113152fa8dc793cc9480a5f0224
alpardfm <tokferdian100@gmail.com>
2024-07-20 17:43:43 +0700

    add header

---
+++ header/header.go
@@ -0,0 +1,33 @@
+package header
+
+const (
+	// Header keys
+	KeyRequestID      string = "x-request-id"
+	KeyAuthorization  string = "authorization"
+	KeyUserAgent      string = "user-agent"
+	KeyContentType    string = "content-type"
+	KeyContentAccept  string = "accept"
+	KeyAcceptLanguage string = "accept-language"
+	KeyCacheControl   string = "cache-control"
+
+	// Content type. Specifying the payload in the request
+	ContentTypeJSON string = "application/json"
+	ContentTypeXML  string = "application/xml"
+	ContentTypeForm string = "application/x-www-form-urlencoded"
+
+	// Accepting media. Specifying the types of requested media (in the response)
+	// See here: https://en.wikipedia.org/wiki/Content_negotiation
+	MediaTextPlain string = "text/plain"
+	MediaTextHTML  string = "text/html"
+	MediaTextCSV   string = "text/csv"
+	MediaTextXML   string = "text/xml"
+
+	MediaImageGIF  string = "image/gif"
+	MediaImageJPEG string = "image/jpeg"
+	MediaImagePNG  string = "image/png"
+	MediaImageWEBP string = "image/webp"
+
+	// Cache control
+	CacheControlNoCache string = "no-cache"
+	CacheControlNoStore string = "no-store"
+)
